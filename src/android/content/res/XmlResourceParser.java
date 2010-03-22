814b70f1d5a71d0a1b3565547ace0293334ace9d
Ryszard Wiśniewski <brut.alll@gmail.com>
2010-03-22 16:17:44 +0100

    +XmlResourceParser +AttributeSet

---
+++ src/android/content/res/XmlResourceParser.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.content.res;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import android.util.AttributeSet;
+
+/**
+ * The XML parsing interface returned for an XML resource.  This is a standard
+ * XmlPullParser interface, as well as an extended AttributeSet interface and
+ * an additional close() method on this interface for the client to indicate
+ * when it is done reading the resource.
+ */
+public interface XmlResourceParser extends XmlPullParser, AttributeSet {
+    /**
+     * Close this interface to the resource.  Calls on the interface are no
+     * longer value after this call.
+     */
+    public void close();
+}
+
---
+++ src/android/util/AttributeSet.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 Android4ME
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.util;
+
+/**
+ * @author Dmitry Skiba
+ *
+ */
+public interface AttributeSet {
+    int getAttributeCount();
+    String getAttributeName(int index);
+    String getAttributeValue(int index);
+    String getPositionDescription();
+    int getAttributeNameResource(int index);
+    int getAttributeListValue(int index,String options[],int defaultValue);
+    boolean getAttributeBooleanValue(int index,boolean defaultValue);
+    int getAttributeResourceValue(int index,int defaultValue);
+    int getAttributeIntValue(int index,int defaultValue);
+    int getAttributeUnsignedIntValue(int index,int defaultValue);
+    float getAttributeFloatValue(int index,float defaultValue);
+    String getIdAttribute();
+    String getClassAttribute();
+    int getIdAttributeResourceValue(int index);
+    int getStyleAttribute();
+    String getAttributeValue(String namespace, String attribute);
+    int getAttributeListValue(String namespace,String attribute,String options[],int defaultValue);
+    boolean getAttributeBooleanValue(String namespace,String attribute,boolean defaultValue);
+    int getAttributeResourceValue(String namespace,String attribute,int defaultValue);
+    int getAttributeIntValue(String namespace,String attribute,int defaultValue);
+    int getAttributeUnsignedIntValue(String namespace,String attribute,int defaultValue);
+    float getAttributeFloatValue(String namespace,String attribute,float defaultValue);
+
+    //TODO: remove
+    int getAttributeValueType(int index);
+    int getAttributeValueData(int index);
+}
