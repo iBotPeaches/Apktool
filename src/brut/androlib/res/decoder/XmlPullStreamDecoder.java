2973601ee955a2aa65ee426c6103ac4604623e43
Ryszard Wiśniewski <brut.alll@gmail.com>
2010-03-22 16:56:38 +0100

    +XmlPullStreamDecoder

---
+++ src/brut/androlib/res/decoder/XmlPullStreamDecoder.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright 2010 Ryszard Wiśniewski <brut.alll@gmail.com>.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package brut.androlib.res.decoder;
+
+import brut.androlib.AndrolibException;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.xmlpull.v1.*;
+import org.xmlpull.v1.wrapper.*;
+
+/**
+ * @author Ryszard Wiśniewski <brut.alll@gmail.com>
+ */
+public class XmlPullStreamDecoder implements ResStreamDecoder {
+    public XmlPullStreamDecoder(XmlPullParser parser,
+            XmlSerializer serializer) {
+        this.mParser = parser;
+        this.mSerial = serializer;
+    }
+
+    public void decode(InputStream in, OutputStream out)
+            throws AndrolibException {
+        try {
+            XmlPullWrapperFactory factory = XmlPullWrapperFactory.newInstance();
+            XmlPullParserWrapper par = factory.newPullParserWrapper(mParser);
+            XmlSerializerWrapper ser = factory.newSerializerWrapper(mSerial);
+
+            par.setInput(in, null);
+            ser.setOutput(out, null);
+
+            while (par.nextToken() != XmlPullParser.END_DOCUMENT) {
+                ser.event(par);
+            }
+            ser.flush();
+            in.close();
+            out.close();
+        } catch (XmlPullParserException ex) {
+            mLogger.log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            mLogger.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private final XmlPullParser mParser;
+    private final XmlSerializer mSerial;
+
+    private final static Logger mLogger =
+        Logger.getLogger(XmlPullStreamDecoder.class.getName());
+}
