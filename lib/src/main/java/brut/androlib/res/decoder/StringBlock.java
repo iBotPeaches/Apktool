840db7357d17c3e5af656587dce4935e512c340f
Ryszard Wiśniewski <brut.alll@gmail.com>
2011-01-17 22:31:41 +0100

    Reformatted license headers.

--- lib/src/main/java/brut/androlib/res/decoder/StringBlock.java
+++ lib/src/main/java/brut/androlib/res/decoder/StringBlock.java
@@ -1,5 +1,5 @@
-/*
- *  Copyright 2010 Ryszard Wiśniewski <brut.alll@gmail.com>.
+/**
+ *  Copyright 2011 Ryszard Wiśniewski <brut.alll@gmail.com>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,8 +12,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  under the License.
  */
+
 package brut.androlib.res.decoder;
 
 import brut.androlib.res.AndrolibResources;
