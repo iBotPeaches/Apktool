84a097252627a83cc61aa35b9d1603cf46e14d7d
Ryszard Wiśniewski <brut.alll@gmail.com>
2011-05-15 16:51:43 +0200

    +apktool.properties +ApktoolProperties

---
+++ apktool-lib/src/main/java/brut/androlib/ApktoolProperties.java
@@ -0,0 +1,55 @@
+/**
+ *  Copyright 2011 Ryszard Wiśniewski <brut.alll@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package brut.androlib;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * @author Ryszard Wiśniewski <brut.alll@gmail.com>
+ */
+public class ApktoolProperties {
+    public static String get(String key) {
+        return get().getProperty(key);
+    }
+    
+    public static Properties get() {
+        if (sProps == null) {
+            loadProps();
+        }
+        return sProps;
+    }
+
+    private static void loadProps() {
+        InputStream in = ApktoolProperties.class
+                .getResourceAsStream("apktool.properties");
+        sProps = new Properties();
+        try {
+            sProps.load(in);
+            in.close();
+        } catch (IOException ex) {
+            LOGGER.warning("Can't load properties.");
+        }
+    }
+    
+    private static Properties sProps;
+    
+    private static final Logger LOGGER =
+            Logger.getLogger(ApktoolProperties.class.getName());
+}
