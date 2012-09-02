962dc63442f1550ec0a86ed532bfe8ed116a848f
Connor Tumbleson <connor.tumbleson@gmail.com>
2012-09-02 01:14:34 -0500

    Changing from DIFFPRE -> FULLMERGE.

--- apktool-lib/src/main/java/brut/androlib/src/SmaliDecoder.java
+++ apktool-lib/src/main/java/brut/androlib/src/SmaliDecoder.java
@@ -44,7 +44,7 @@ public class SmaliDecoder {
             baksmali.disassembleDexFile(mApkFile.getAbsolutePath(),
                 new DexFile(mApkFile), false, mOutDir.getAbsolutePath(), null,
                 null, null, false, true, true, true, false, false, 
-                mDebug ? main.DIFFPRE : 0, false, mDebug ? true : false, null);
+                mDebug ? main.FULLMERGE : 0, false, mDebug ? true : false, null);
         } catch (IOException ex) {
             throw new AndrolibException(ex);
         }
