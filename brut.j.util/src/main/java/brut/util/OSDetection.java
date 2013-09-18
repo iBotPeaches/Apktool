5fa3c2d2972b414fdfa5f8461927d4cbb21e3507
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-09-17 21:52:35 -0500

    add returnOS() to OS, to help identify unknown aapt OS's

--- brut.j.util/src/main/java/brut/util/OSDetection.java
+++ brut.j.util/src/main/java/brut/util/OSDetection.java
@@ -17,17 +17,21 @@
 package brut.util;
 
 public class OSDetection {
-  private static String OS = System.getProperty("os.name").toLowerCase();
-  
-  public static boolean isWindows() {
-    return (OS.indexOf("win") >= 0);
-  }
-  
-  public static boolean isMacOSX() {
-    return (OS.indexOf("mac") >= 0);
-  }
-  
-  public static boolean isUnix() {
-    return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 || (OS.indexOf("sunos") >= 0));
-  }
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMacOSX() {
+        return (OS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isUnix() {
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 || (OS.indexOf("sunos") >= 0));
+    }
+
+    public static String returnOS() {
+        return OS;
+    }
 }
