f85dc478b2c1cc4038be509eab0fbf85432b0d40
Connor Tumbleson <connor.tumbleson@gmail.com>
2014-02-10 07:17:56 -0600

    Github pr #81 - correctly handle non-URI characters in filename

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/Androlib.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/Androlib.java
@@ -217,9 +217,10 @@ public class Androlib {
         // options.setIndent(4);
         Yaml yaml = new Yaml(options);
 
-        FileWriter writer = null;
+        Writer writer = null;
         try {
-            writer = new FileWriter(new File(mOutDir, "apktool.yml"));
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(new File(mOutDir, "apktool.yml")), "UTF-8"));
             yaml.dump(meta, writer);
         } catch (IOException ex) {
             throw new AndrolibException(ex);
@@ -556,7 +557,7 @@ public class Androlib {
 
                 // create filesystem
                 Path path = Paths.get(outFile.getAbsolutePath());
-                URI apkFileSystem = new URI("jar", path.toUri().toString(), null);
+                URI apkFileSystem = new URI("jar", outFile.toURI().toString(), null);
 
                 // loop through files inside
                 for (Map.Entry<String,String> entry : files.entrySet()) {
