5d405fa842e22e7516df493c7091392ed94fe374
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-04-18 08:44:25 -0500

    add a unit-test for provider attr changing

---
+++ brut.apktool/apktool-lib/src/test/java/brut/androlib/ProviderAttributeTest.java
@@ -0,0 +1,91 @@
+/**
+ *  Copyright 2014 Ryszard Wiśniewski <brut.alll@gmail.com>
+ *  Copyright 2014 Connor Tumbleson <connor.tumbleson@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package brut.androlib;
+
+import brut.androlib.res.util.ExtFile;
+import brut.common.BrutException;
+import brut.directory.DirectoryException;
+import brut.util.OS;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ProviderAttributeTest {
+
+    @BeforeClass
+    public static void beforeClass() throws BrutException {
+        sTmpDir = new ExtFile(OS.createTempDirectory());
+        TestUtils.copyResourceDir(ProviderAttributeTest.class, "brut/apktool/issue636/", sTmpDir);
+    }
+
+    @AfterClass
+    public static void afterClass() throws BrutException {
+        OS.rmdir(sTmpDir);
+    }
+
+    @Test
+    public void isProviderStringReplacementWorking() throws BrutException, IOException {
+        String apk = "issue636.apk";
+
+        // decode issue636.apk
+        ApkDecoder apkDecoder = new ApkDecoder(new File(sTmpDir + File.separator + apk));
+        apkDecoder.setOutDir(new File(sTmpDir + File.separator + apk + ".out"));
+        apkDecoder.decode();
+
+        // build issue636
+        ExtFile testApk = new ExtFile(sTmpDir, apk + ".out");
+        new Androlib().build(testApk, null);
+        String newApk = apk + ".out" + File.separator + "dist" + File.separator + apk;
+        assertTrue(fileExists(newApk));
+
+        // decode issues636 again
+        apkDecoder = new ApkDecoder(new File(sTmpDir + File.separator + newApk));
+        apkDecoder.setOutDir(new File(sTmpDir + File.separator + apk + ".out.two"));
+        apkDecoder.decode();
+
+        String expected = replaceNewlines("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>\n" +
+                "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"com.ibotpeaches.issue636\" platformBuildVersionCode=\"22\" platformBuildVersionName=\"5.1-1756733\">\n" +
+                "    <application android:allowBackup=\"true\" android:debuggable=\"true\" android:icon=\"@mipmap/ic_launcher\" android:label=\"@string/app_name\" android:theme=\"@style/AppTheme\">\n" +
+                "        <provider android:authorities=\"com.ibotpeaches.issue636.Provider\" android:exported=\"false\" android:grantUriPermissions=\"true\" android:label=\"@string/app_name\" android:multiprocess=\"false\" android:name=\"com.ibotpeaches.issue636.Provider\"/>\n" +
+                "        <provider android:authorities=\"com.ibotpeaches.issue636.ProviderTwo\" android:exported=\"false\" android:grantUriPermissions=\"true\" android:label=\"@string/app_name\" android:multiprocess=\"false\" android:name=\"com.ibotpeaches.issue636.ProviderTwo\"/>\n" +
+                "    </application>\n" +
+                "</manifest>");
+
+
+        byte[] encoded = Files.readAllBytes(Paths.get(sTmpDir + File.separator + apk + ".out.two" + File.separator + "AndroidManifest.xml"));
+        String obtained = replaceNewlines(new String(encoded));
+        assertEquals(expected, obtained);
+    }
+
+    private boolean fileExists(String filepath) {
+        return Files.exists(Paths.get(sTmpDir.getAbsolutePath() + File.separator + filepath));
+    }
+
+    private String replaceNewlines(String value) {
+        return value.replace("\n", "").replace("\r", "");
+    }
+
+    private static ExtFile sTmpDir;
+}
