5481376d0c743b55e34a2e20c11d786d0a87f59f
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-04-29 08:04:20 -0500

    Prevent appending the current directory to path on every run  - Fixes #927

--- scripts/windows/apktool.bat
+++ scripts/windows/apktool.bat
@@ -1,3 +1,4 @@
 @echo off
-set PATH=%CD%;%PATH%;
+if "%PATH_BASE%" == "" set PATH_BASE=%PATH%
+set PATH=%CD%;%PATH_BASE%;
 java -jar -Duser.language=en "%~dp0\apktool.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
