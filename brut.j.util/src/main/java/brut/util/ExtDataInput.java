894da2033ba5dccec385a619b0e9177d01f2efe7
Connor Tumbleson <me@connortumbleson.com>
2016-10-13 09:00:08 -0400

    If obtained value is smaller than expected, retry
     - fixes #1332
     - thanks xpirt

--- brut.j.util/src/main/java/brut/util/ExtDataInput.java
+++ brut.j.util/src/main/java/brut/util/ExtDataInput.java
@@ -69,7 +69,7 @@ public class ExtDataInput extends DataInputDelegate {
     public void skipCheckChunkTypeInt(int expected, int possible) throws IOException {
         int got = readInt();
 
-        if (got == possible) {
+        if (got == possible || got < expected) {
             skipCheckChunkTypeInt(expected, -1);
         } else if (got != expected) {
             throw new IOException(String.format("Expected: 0x%08x, got: 0x%08x", expected, got));
