481299c748bb7d692cd9da4971a7879c17234e35
Connor Tumbleson <me@connortumbleson.com>
2017-08-25 06:36:50 -0400

    Update Windows scripts for unicode support
     - fixes #1595

--- scripts/windows/apktool.bat
+++ scripts/windows/apktool.bat
@@ -1,4 +1,5 @@
 @echo off
 if "%PATH_BASE%" == "" set PATH_BASE=%PATH%
 set PATH=%CD%;%PATH_BASE%;
-java -jar -Duser.language=en "%~dp0\apktool.jar" %*
+chcp 65001 2>nul >nul
+java -jar -Duser.language=en -Dfile.encoding=UTF8 "%~dp0\apktool.jar" %*
