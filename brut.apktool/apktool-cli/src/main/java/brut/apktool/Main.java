14c5f87e6ee7b5ab3b1fcab4cb4eefe03322ae74
Andrei Conache <xpirt@users.noreply.github.com>
2017-08-27 16:21:54 +0200

    fix indentation

--- brut.apktool/apktool-cli/src/main/java/brut/apktool/Main.java
+++ brut.apktool/apktool-cli/src/main/java/brut/apktool/Main.java
@@ -265,7 +265,7 @@ public class Main {
 
         // create options
         Option versionOption = Option.builder("version")
-		        .longOpt("version")
+                .longOpt("version")
                 .desc("prints the version then exits")
                 .build();
 
