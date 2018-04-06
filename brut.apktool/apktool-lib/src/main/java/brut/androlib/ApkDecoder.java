4d8922868616b85180507f899f95b7edaebb8687
Connor Tumbleson <connor@sourcetoad.com>
2018-04-06 14:58:42 -0400

    Revert "decode: store uncompressed assets to new property"
    This reverts commit 14eedde165623e0f28bad036b3ce8af883a0a635.

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkDecoder.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkDecoder.java
@@ -418,7 +418,7 @@ public class ApkDecoder {
 
     private void putFileCompressionInfo(MetaInfo meta) throws AndrolibException {
         if (mUncompressedFiles != null && !mUncompressedFiles.isEmpty()) {
-            meta.noCompressAssets = mUncompressedFiles;
+            meta.doNotCompress = mUncompressedFiles;
         }
     }
 
