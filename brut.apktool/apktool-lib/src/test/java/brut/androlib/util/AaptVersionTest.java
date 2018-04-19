3879b90f7610d0465ba6624a51e9117f60a11b51
Connor Tumbleson <connor@sourcetoad.com>
2018-04-19 17:10:10 -0400

    test: add test for various aapt2 version strings

---
+++ brut.apktool/apktool-lib/src/test/java/brut/androlib/util/AaptVersionTest.java
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (C) 2018 Ryszard Wiśniewski <brut.alll@gmail.com>
+ *  Copyright (C) 2018 Connor Tumbleson <connor.tumbleson@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package brut.androlib.util;
+
+import brut.common.BrutException;
+import brut.util.AaptManager;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AaptVersionTest {
+
+    @Test
+    public void testAapt2Iterations() throws BrutException {
+        assertEquals(2, AaptManager.getAppVersionFromString("Android Asset Packaging Tool (aapt) 2:17"));
+        assertEquals(2, AaptManager.getAppVersionFromString("Android Asset Packaging Tool (aapt) 2.17"));
+        assertEquals(1, AaptManager.getAppVersionFromString("Android Asset Packaging Tool, v0.9"));
+    }
+}
