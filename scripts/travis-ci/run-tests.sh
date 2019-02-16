37e700424a27dc3dc7b4a2ddc94b601176164fb1
Connor Tumbleson <connor.tumbleson@gmail.com>
2019-02-16 13:43:23 -0500

    build: update jdk on windows from 191 to 201

--- scripts/travis-ci/run-tests.sh
+++ scripts/travis-ci/run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_191/bin"
+    export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_201/bin"
     ./gradlew.bat build shadowJar proguard
 else
     ./gradlew build shadowJar proguard
