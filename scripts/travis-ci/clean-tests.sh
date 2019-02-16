8b4d2d9178a21edc376a5203bcfb6e6523fa1019
Connor Tumbleson <connor.tumbleson@gmail.com>
2019-02-16 14:25:56 -0500

    build: also patch the clean-tests for not completed win builds

--- scripts/travis-ci/clean-tests.sh
+++ scripts/travis-ci/clean-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    export GRADLE_OPTS=-Dorg.gradle.daemon=false
     choco install jdk8
     export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_201/bin"
     ./gradlew.bat clean
