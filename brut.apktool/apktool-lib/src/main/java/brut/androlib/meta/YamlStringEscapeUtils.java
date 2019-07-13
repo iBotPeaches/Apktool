25a1cb02ef5d6f463f412dff75d22e758ba81460
Connor Tumbleson <connor.tumbleson@gmail.com>
2019-07-13 12:19:41 -0400

    fix: License 2019 update

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/meta/YamlStringEscapeUtils.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/meta/YamlStringEscapeUtils.java
@@ -1,6 +1,6 @@
 /**
- *  Copyright (C) 2018 Ryszard Wiśniewski <brut.alll@gmail.com>
- *  Copyright (C) 2018 Connor Tumbleson <connor.tumbleson@gmail.com>
+ *  Copyright (C) 2019 Ryszard Wiśniewski <brut.alll@gmail.com>
+ *  Copyright (C) 2019 Connor Tumbleson <connor.tumbleson@gmail.com>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
