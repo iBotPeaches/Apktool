49b1fc126478b772429cc298b54b55727d7b8bdf
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-07-15 16:22:42 -0400

    fix: add more prism languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,6 +127,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['java', 'smali', 'bash'],
       },
     }),
 };
