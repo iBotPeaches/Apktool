54836509ed39cd54eb5b53fc627eb0dfbed5d8af
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-07-23 11:20:12 -0400

    fix: correct detection of sparse/not-sparse applications (#3201)
    * test: add failing/passing sparse/not-sparse test
    
    * fix: replace sparse/not applications w/ official sample
    
     -ref: https://github.com/aosp-mirror/platform_frameworks_base/commit/c8f71aa67ea599cb80205496cb67e9e7a121299c
    
    * fix: properly detect sparse applications during disassembly
    
    * refactor: remove check on bool set for sparse resources

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/res/decoder/ARSCDecoder.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/res/decoder/ARSCDecoder.java
@@ -126,12 +126,6 @@ public class ARSCDecoder {
             addMissingResSpecs();
         }
 
-        // We've detected sparse resources, lets record this so we can rebuild in that same format (sparse/not)
-        // with aapt2. aapt1 will ignore this.
-        if (! mResTable.getSparseResources()) {
-            mResTable.setSparseResources(true);
-        }
-
         return pkgs.toArray(new ResPackage[0]);
     }
 
@@ -276,6 +270,9 @@ public class ARSCDecoder {
 
         if ((typeFlags & 0x01) != 0) {
             LOGGER.fine("Sparse type flags detected: " + mTypeSpec.getName());
+
+            // We've detected sparse resources, lets record this so we can rebuild in that same format
+            mResTable.setSparseResources(true);
         }
 
         HashMap<Integer, Integer> entryOffsetMap = new LinkedHashMap<>();
---
+++ brut.apktool/apktool-lib/src/test/java/brut/androlib/decode/SparseFlagTest.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (C) 2010 Ryszard Wiśniewski <brut.alll@gmail.com>
+ *  Copyright (C) 2010 Connor Tumbleson <connor.tumbleson@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package brut.androlib.decode;
+
+import brut.androlib.*;
+import brut.androlib.apk.ApkInfo;
+import brut.common.BrutException;
+import brut.directory.ExtFile;
+import brut.util.OS;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SparseFlagTest extends BaseTest {
+
+    @Before
+    public void beforeClass() throws Exception {
+        TestUtils.cleanFrameworkFile();
+        sTmpDir = new ExtFile(OS.createTempDirectory());
+        sTestOrigDir = new ExtFile(sTmpDir, "sparse-orig");
+        sTestNewDir = new ExtFile(sTmpDir, "sparse-new");
+        LOGGER.info("Unpacking sparse.apk && not-sparse.apk...");
+        TestUtils.copyResourceDir(SparseFlagTest.class, "decode/sparse", sTestOrigDir);
+    }
+
+    @After
+    public void afterClass() throws BrutException {
+        OS.rmdir(sTmpDir);
+    }
+
+    @Test
+    public void decodeWithExpectationOfSparseResources() throws BrutException, IOException {
+        File testApk = new File(sTestOrigDir, "sparse.apk");
+
+        LOGGER.info("Decoding sparse.apk...");
+        Config config = Config.getDefaultConfig();
+
+        ApkDecoder apkDecoder = new ApkDecoder(config, testApk);
+        ApkInfo apkInfo = apkDecoder.decode(sTestNewDir);
+
+        assertTrue("Expecting sparse resources", apkInfo.sparseResources);
+
+        LOGGER.info("Building sparse.apk...");
+        new ApkBuilder(config, sTestNewDir).build(testApk);
+    }
+
+    @Test
+    public void decodeWithExpectationOfNoSparseResources() throws BrutException, IOException {
+        File testApk = new File(sTestOrigDir, "not-sparse.apk");
+
+        LOGGER.info("Decoding not-sparse.apk...");
+        Config config = Config.getDefaultConfig();
+
+        ApkDecoder apkDecoder = new ApkDecoder(config, testApk);
+        ApkInfo apkInfo = apkDecoder.decode(sTestNewDir);
+
+        assertFalse("Expecting not-sparse resources", apkInfo.sparseResources);
+
+        LOGGER.info("Building not-sparse.apk...");
+        new ApkBuilder(config, sTestNewDir).build(testApk);
+    }
+}
