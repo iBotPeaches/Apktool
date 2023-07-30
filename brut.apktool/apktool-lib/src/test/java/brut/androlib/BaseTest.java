6171796bb06e0700e7685e402c043945b93a1856
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-07-30 13:22:08 -0400

    feat: opt into xxe protection for test suite

--- brut.apktool/apktool-lib/src/test/java/brut/androlib/BaseTest.java
+++ brut.apktool/apktool-lib/src/test/java/brut/androlib/BaseTest.java
@@ -115,6 +115,8 @@ public class BaseTest {
             Reader control = new FileReader(new File(sTestOrigDir, path));
             Reader test = new FileReader(new File(sTestNewDir, path));
 
+            XMLUnit.setEnableXXEProtection(true);
+
             if (qualifier == null) {
                 XMLUnit.setIgnoreWhitespace(true);
                 XMLUnit.setIgnoreAttributeOrder(true);
