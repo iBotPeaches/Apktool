09b7fd4e0774a01916371b78bba3df21b238fd40
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-08-04 06:45:21 -0400

    fix: wire up proguard

--- brut.apktool/apktool-cli/build.gradle.kts
+++ brut.apktool/apktool-cli/build.gradle.kts
@@ -13,28 +13,35 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import proguard.gradle.ProGuardTask
 
-val proguardGradleVersion: String by rootProject.extra
 val commonsCliVersion: String by rootProject.extra
+val apktoolVersion: String by rootProject.extra
 
 plugins {
     id("com.github.johnrengelman.shadow")
     application
 }
 
-dependencies {
-    compileOnly("com.guardsquare:proguard-gradle:$proguardGradleVersion")
-}
-
-configurations {
-    compileClasspath {
-        exclude(group = "com.android.tools.build")
+// Buildscript is deprecated, but the alternative approach does not support expanded properties
+// https://github.com/gradle/gradle/issues/9830
+// So we must hard-code the version here.
+buildscript {
+    dependencies {
+        // Proguard doesn't support plugin DSL - https://github.com/Guardsquare/proguard/issues/225
+        classpath("com.guardsquare:proguard-gradle:7.3.2")
     }
 }
 
 dependencies {
     implementation("commons-cli:commons-cli:$commonsCliVersion")
     implementation(project(":brut.apktool:apktool-lib"))
+}
+
+configurations {
+    compileClasspath {
+        exclude(group = "com.android.tools.build")
+    }
 }
 
 application {
@@ -48,3 +55,36 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = "brut.apktool.Main"
     }
 }
+
+tasks.register<ProGuardTask>("proguard") {
+    dependsOn("shadowJar")
+    injars(tasks.named("shadowJar").get().outputs.files)
+
+    val javaHome = System.getProperty("java.home")
+    if (JavaVersion.current() <= JavaVersion.VERSION_1_8) {
+        libraryjars("$javaHome/lib/jce.jar")
+        libraryjars("$javaHome/lib/rt.jar")
+    } else {
+        libraryjars(mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class"),
+            {
+                "$javaHome/jmods/"
+            }
+        )
+    }
+
+    dontobfuscate()
+    dontoptimize()
+
+    keep("class brut.apktool.Main { public static void main(java.lang.String[]); }")
+    keepclassmembers("enum * { public static **[] values(); public static ** valueOf(java.lang.String); }")
+    dontwarn("com.google.common.base.**")
+    dontwarn("com.google.common.collect.**")
+    dontwarn("com.google.common.util.**")
+    dontwarn("javax.xml.xpath.**")
+    dontnote("**")
+
+    val outPath = "build/libs/apktool-cli-$apktoolVersion.jar"
+    outjars(outPath)
+}
+
+tasks.getByPath(":release").dependsOn("proguard")
