d2599874d8e612d38a7b4a115e564c269dcb7d0c
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-10-08 14:10:20 -0400

    build: start new dev cycle (2.9.1-SNAPSHOT)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.io.ByteArrayOutputStream
 
-val version = "2.9.0"
-val suffix = ""
+val version = "2.9.1"
+val suffix = "SNAPSHOT"
 
 // Strings embedded into the build.
 var gitRevision by extra("")
