d365a77caacde29d0a7bb927033d871c2a3d38c7
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2024-01-05 07:20:43 -0500

    feat: 2.9.2 (#3487)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,8 +91,8 @@ const config = {
           {to: '/docs/install', label: 'Install', position: 'left'},
           {to: '/blog', label: 'Releases', position: 'left'},
           {
-            href: 'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.1.jar',
-            label: 'Download 2.9.1',
+            href: 'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.2.jar',
+            label: 'Download 2.9.2',
             position: 'right',
           },
           {
