f2f693b7ea41999d8af82994c9b3f00aa8835a8b
IcyClawz <igoreisberg@gmail.com>
2024-10-05 23:49:05 +0300

    refactor: clean up external pull parser and introduce brut.j.xml
    We have no need for an XML pull parser in the project,
    it was only used for testing, which is now done with XPath.
    
    The external xpp3 library from org.ogce is obsolete and has
    the issue of including javax.xml.namespace.QName which conflicts
    with the JRE implementation that exists for a very long time now.
    This makes direct usages of QName produce very obscure NPEs that
    took me hours to figure out. This patch will allow further
    optimization that is WIP.
    The external library was replaced by the basic xmlpull API.
    
    The MXSerializer has been cleaned and the features used by apktool
    have been integrated into the custom implementation, now part of
    a separate module called brut.j.xml.
    Writing has been optimized by buffering write operations, inspired
    by KXmlSerializer used by Android itself.
    
    A class XmlPullUtils also written that allows copying from a
    XmlPullParser into a XmlSerializer with or without an EventHandler.
    We use it for AndroidManifestPullStreamDecoder (with EventHandler,
    to allow omitting the uses-sdk tag), and for ResXmlPullStreamDecoder
    (direct copy, without EventHandler).
    
    saveDocument in ResXmlPatcher was tweaked to output proper output -
    a new line after declaration and a new line after root element's
    end tag.
    
    TL;DR mostly behind the scene refactor, no end user changes.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "apktool-cli"
-include("brut.j.common", "brut.j.util", "brut.j.dir", "brut.apktool:apktool-lib", "brut.apktool:apktool-cli")
+include("brut.j.common", "brut.j.util", "brut.j.dir", "brut.j.xml", "brut.apktool:apktool-lib", "brut.apktool:apktool-cli")
 
 dependencyResolutionManagement {
     versionCatalogs {
