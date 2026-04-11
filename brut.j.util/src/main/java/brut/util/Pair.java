cbca15de46df60c8d41570347c199b0302754392
X1nto <burtkni@yandex.ru>
2026-04-11 16:14:33 +0400

    Use a custom Pair implementation with better hashCode generation

---
+++ brut.j.util/src/main/java/brut/util/Pair.java
@@ -0,0 +1,48 @@
+package brut.util;
+
+import java.util.Objects;
+
+public final class Pair<L, R> {
+
+    private final L mLeft;
+    private final R mRight;
+
+    private Pair(L left, R right) {
+        mLeft = left;
+        mRight = right;
+    }
+
+    public static <A, B> Pair<A, B> of(A left, B right) {
+        return new Pair<>(left, right);
+    }
+
+    public L getLeft() {
+        return mLeft;
+    }
+
+    public R getRight() {
+        return mRight;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof Pair) {
+            Pair<?, ?> other = (Pair<?, ?>) obj;
+            return Objects.equals(mLeft, other.mLeft) && Objects.equals(mRight, other.mRight);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Objects.hashCode(mLeft) + Objects.hashCode(mRight);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + mLeft + ", " + mRight + ")";
+    }
+}
