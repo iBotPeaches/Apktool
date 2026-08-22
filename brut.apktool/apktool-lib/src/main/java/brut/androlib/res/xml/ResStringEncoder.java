6dd8a500bdc917b25f6ecc34b30a1ddc5bdeac66
iMiKE <M-I-B@yandex.ru>
2026-08-22 18:42:40 +0700

    Fix empty raw string encoding

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/res/xml/ResStringEncoder.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/res/xml/ResStringEncoder.java
@@ -161,6 +161,9 @@ public final class ResStringEncoder {
 
         StringBuilder out = new StringBuilder(len * 2);
         appendEscapedString(out, str, 0, len, attrType, false);
+        if (out.length() == 0) {
+            return "";
+        }
 
         // Raw strings might get encoded as typed values in edge cases. We skip this if the string has been quoted.
         if (out.charAt(0) != '"' && isAmbiguousString(out, attrType)) {
